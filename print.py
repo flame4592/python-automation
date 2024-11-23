0c894cfa75191732abe39356c5d5020449165edc
adarsh raj <rajadarsh4592@gmail.com>
2024-11-24 01:35:27 +0530

    learning to print , len , string , assign values

---
+++ print.py
@@ -0,0 +1,13 @@
+print ( "hello" + "world")
+print ( "hell0" + str(29))
+print ( "what is your name ")
+
+myName= input()
+
+print ( "nice to meet you "  + myName)
+ageLen = (len(myName)) 
+print ( "the length of your name is :- " + str(ageLen) )
+
+print ( "what is your age")
+myAge = input ()
+print (" you will be " + str(int(myAge) + 1 ) + " in 1 year" )
