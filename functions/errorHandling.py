a32e5e04ab79bc143bb8208b7c9c2ff02c016e00
adarsh raj <rajadarsh4592@gmail.com>
2024-11-25 04:23:14 +0530

    functions with error handling and variables

---
+++ functions/errorHandling.py
@@ -0,0 +1,11 @@
+# Errors can be handled with try and except statements. 
+def spam(divideBy):
+    try:
+        return 42 / divideBy
+    except ZeroDivisionError:
+        print('Error: Invalid argument.')
+
+print(spam(2))
+print(spam(12))
+print(spam(0))
+print(spam(1))
---
+++ functions/variables.py
@@ -0,0 +1,5 @@
+def spam():
+    print (eggs)
+
+eggs = 43
+spam() 
